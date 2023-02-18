f5cebe7d5a543bc59967e5fe0ce2d2c14d092790
Jarod Anjelo Lustre <50737321+jard-io@users.noreply.github.com>
2023-02-18 01:23:07 -0500

    Add files via upload

--- gather_metadata.sh
+++ gather_metadata.sh
@@ -13,16 +13,74 @@ else
     fi
 fi
 
-#rest
+#this function is responsible for creating the thumbnails of different sizes
+function createThumbnail(){
+    #resolution
+    res=$1
+    #loop condition
+    size=$1
+    #get file name without extension
+    fName=$(cut -d. -f1 <<< $2)
+    #get extension
+    fExtension=$(cut -d. -f2 <<< $2)
+    #test fName and fExt are correct
+    echo "$fName    $fExtension"
+    #get file directory of image
+    fDir=$3
+    #get directory to create thumbnails to
+    fDirT=$4
+
+    while [ $size -gt 0 ]
+    do
+        #create new filename
+        newName=$fDirT
+        newName+="/$fName"
+        newName+="-$size"
+        newName+=".$fExtension"
+        echo $fDir
+        echo $newName
+        #convert
+        (convert $fDir -resize ${size}x${size} $newName)
+        #decrement loop
+        if [ $size -eq 512 ] ; then
+            size=$(($size-256))
+        else
+            size=$(($size-128))
+        fi
+    done
+
+}
+
+#main
 for i in $(find $dir -type f \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg -o -iname \*.tiff -o -iname \*.tif -o -iname \*.gif -o -iname \*.bmp \))
 do 
+    #directory where file is stored WITHOUT filename
     tempDIR=$(dirname $i)
-    tempDIR+="/.thumbs" 
+    #directory where .thumbs is created
+    dirThumbs=$tempDIR
+    dirThumbs+="/.thumbs"
+    #filename with extension 
     fileName=$(basename $i)
-    echo $fileName
-    if [ ! -d "$tempDIR" ] ; then
-        mkdir $tempDIR
+
+    #get width
+    W=$(identify -format '%w' $i)
+
+    #get height
+    H=$(identify -format '%h' $i)
+    echo $i $W x $H
+    # !!! - make sure to implement check so that images in .thumbs arent processed (not implemented yet)
+    if [ ! -d "$dirThumbs" ] ; then
+        mkdir $dirThumbs
     fi
+    #conditions for creating thumbnails
+    if [[ $W -gt 512 || $H -gt 512 ]] ; then
+        createThumbnail 512 $fileName $i $dirThumbs
+    elif [[ $W -gt 256 && $W -le 512 ]] || [[ $H -gt 256 && $H -le 512 ]] ; then
+        createThumbnail 256 $fileName $i $dirThumbs
+    elif [[ $W -gt 128 && $W -le 256 ]] || [[ $H -gt 128 && $H -le 256 ]] ; then
+        createThumbnail 128 $fileName $i $dirThumbs
+    fi
+
 done
 
 
